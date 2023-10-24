cf7db2c76aab662cdf7497ff1cb1f3f4f99daf68
Noel Guillen <noelblas274@gmail.com>
2023-10-24 02:05:49 -0400

    added new folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="card card-side bg-base-100 shadow-xl">
-  <figure><img src="https://render.fineartamerica.com/images/rendered/default/greeting-card/images/artworkimages/medium/3/5-winnie-the-pooh-piglet-amalia-oktaviani-transparent.png?&targetx=100&targety=0&imagewidth=499&imageheight=500&modelwidth=700&modelheight=500&backgroundcolor=5a92e0&orientation=0" alt="Movie"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">New movie is released!</h2>
-    <p>Click the button to watch on Jetflix app.</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Watch</button>
-    </div>
-  </div>
-</div>
+    <BrowserRouter>
+     <div className="flex flex-col justify-between h-screen">
+      <Navbar />
+      <main className='container px-3 pb-12 mx-auto'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/notfound' element={<NotFound />} />
+          <Route path='/*' element={<NotFound />} />
+        </Routes>
+      </main>
+      <Footer />
+     </div>
+    </BrowserRouter>
   )
 }
 
